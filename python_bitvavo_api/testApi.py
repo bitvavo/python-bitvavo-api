28ce0e9877992baaf5c852dea7ae8f438cc8bc53
Pavel Baryshnikov <pavel.baryshnikov@bitvavo.com>
2025-05-09 16:41:10 +0200

    chore: add --verbose flag

--- python_bitvavo_api/testApi.py
+++ python_bitvavo_api/testApi.py
@@ -14,14 +14,12 @@ import json
 
 def main():
   bitvavo = Bitvavo({
-    'APIKEY': '<APIKEY>',
-    'APISECRET': '<APISECRET>',
-    'RESTURL': 'https://api.bitvavo.com/v2',
-    'WSURL': 'wss://ws.bitvavo.com/v2/',
-    'ACCESSWINDOW': 10000,
-    'DEBUGGING': False
+    'APIKEY': 'a2c2c44fa4457addbec6b08f264febb0fd102be0d08cd989dd75fb6f1e127385',
+    'APISECRET': 'fd53003af7bd886c5ed0b6630a559e52cdc53c38d19276d27d6ef97158e2c6f35e771a0403315d8e056400d5c016e2123e77d79e4b239d43112cb478811382d4',
+    'RESTURL': 'https://api.dev.vavo.dev/v2',
+    'WSURL': 'wss://edge.dev.internal.vavo.dev/exchange/wsg/v2'
   })
-  testREST(bitvavo)
+  # testREST(bitvavo)
   testWebsockets(bitvavo)
 
 def testREST(bitvavo):
@@ -64,8 +62,10 @@ def testREST(bitvavo):
   # for market in response:
   #   print(json.dumps(market, indent=2))
 
-  # response = bitvavo.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '0.1', 'price': '2000' })
-  # print(json.dumps(response, indent=2))
+  response = bitvavo.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '0.1', 'price': '80000', 'operatorId': 1000 })
+  response = bitvavo.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '0.1', 'price': '80000', 'operatorId': 1000 })
+  response = bitvavo.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '0.1', 'price': '80000', 'operatorId': 1000 })
+  print(json.dumps(response, indent=2))
 
   # response = bitvavo.placeOrder('BTC-EUR', 'sell', 'stopLoss', { amount: '0.1', 'triggerType': 'price', 'triggerReference': 'lastTrade', 'triggerAmount': '5000' })
   # print(json.dumps(response, indent=2))
@@ -73,19 +73,22 @@ def testREST(bitvavo):
   # response = bitvavo.getOrder('BTC-EUR', 'dd055772-0f02-493c-a049-f4356fa0d221')
   # print(json.dumps(response, indent=2))
 
-  # response = bitvavo.updateOrder("BTC-EUR", "dd055772-0f02-493c-a049-f4356fa0d221", { "amount": "0.2" })
+  # time.sleep(5)
+  orderId = response['orderId']
+  # print(orderId)
+  # response = bitvavo.updateOrder("BTC-EUR", response['orderId'], { "amount": "0.2", 'operatorId': 1001  })
   # print(json.dumps(response, indent=2))
 
-  # response = bitvavo.cancelOrder('BTC-EUR', 'dd055772-0f02-493c-a049-f4356fa0d221')
+  # response = bitvavo.cancelOrder('BTC-EUR', orderId, 1002)
   # print(json.dumps(response, indent=2))
 
   # response = bitvavo.getOrders('BTC-EUR')
   # for item in response:
   #   print(json.dumps(item, indent=2))
 
-  # response = bitvavo.cancelOrders({ 'market': 'BTC-EUR' })
-  # for item in response:
-  #   print(json.dumps(item, indent=2))
+  response = bitvavo.cancelOrders({ 'market': 'BTC-EUR', 'operatorId': 1003 })
+  for item in response:
+    print(json.dumps(item, indent=2))
 
   # response = bitvavo.ordersOpen()
   # for item in response:
@@ -117,8 +120,13 @@ def testREST(bitvavo):
   #   print(json.dumps(item, indent=2))
 
 # Normally you would define a separate callback for every function.
+
+orderId = None
 def callback(response):
-  print("Callback:", json.dumps(response, indent=2))
+  dumps = json.dumps(response, indent=2)
+  global orderId
+  orderId = response.get('orderId')
+  print("Callback:", dumps)
 
 def errorCallback(error):
   print("Error callback:", json.dumps(error, indent=2))
@@ -139,12 +147,14 @@ def testWebsockets(bitvavo):
   # websocket.tickerPrice({}, callback)
   # websocket.tickerBook({}, callback)
 
-  # websocket.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '1', 'price': '3000' }, callback)
+  websocket.placeOrder('BTC-EUR', 'buy', 'limit', { 'amount': '1', 'price': '80000', 'operatorId': 1001 }, callback)
   # websocket.getOrder('BTC-EUR', '6d0dffa7-07fe-448e-9928-233821e7cdb5', callback)
-  # websocket.updateOrder('BTC-EUR', '6d0dffa7-07fe-448e-9928-233821e7cdb5', { 'amount': '1.1' }, callback)
-  # websocket.cancelOrder('BTC-EUR', '6d0dffa7-07fe-448e-9928-233821e7cdb5', callback)
+  time.sleep(2)
+  print(orderId)
+  websocket.updateOrder('BTC-EUR', orderId, { 'amount': '1.1', 'operatorId': 1002 }, callback)
+  # websocket.cancelOrder('BTC-EUR', orderId, callback, 1003)
   # websocket.getOrders('BTC-EUR', {}, callback)
-  # websocket.cancelOrders({ 'market': 'BTC-EUR' }, callback)
+  websocket.cancelOrders({ 'market': 'BTC-EUR', 'operatorId': 1004 }, callback)
   # websocket.ordersOpen({}, callback)
 
   # websocket.trades('BTC-EUR', {}, callback)
